9f8030c28f6bfb1dff1d797d5e0df40bf9c33607
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-03-09 20:34:21 +0900

    チーム編集機能の実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,26 @@
 class TeamsController < ApplicationController
+  before_action :authenticate_team!, except: :index
+
   def index
     
+  end
+
+
+
+  def edit
+    @team = Team.find(current_team.id)
+  end
+
+  def update
+    if current_team.update(update_params)
+      redirect_to root_path
+    else
+      render action: :edit
+    end
+  end
+
+  private
+  def update_params
+    params.require(:team).permit(:name, :rep_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :teams
   root to: "teams#index"
+  resources :teams, only: [:index, :edit, :update]
 end
