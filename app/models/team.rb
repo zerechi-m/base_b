90cdfd8b1f605a53aea1930997d2eb1c26172bd5
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-03-09 17:47:22 +0900

    ログイン状態でヘッダー表示分岐

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,7 +5,7 @@ class Team < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :name, uniquness: true, length: {in: 4..20}
+    validates :name, length: {in: 4..20}, uniqueness: true
     validates :rep_name, length: {maximum: 15}, format: { with: /\A[ぁ-ん一-龥]+\z/ }
   end
 end
