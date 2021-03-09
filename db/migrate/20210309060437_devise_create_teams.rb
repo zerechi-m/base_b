5f3ff9c5ec83cdd6d7854ddacaa869216c5e1b00
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-03-09 15:11:46 +0900

    Teamモデルのmigrateファイルの追加

--- db/migrate/20210309060437_devise_create_teams.rb
+++ db/migrate/20210309060437_devise_create_teams.rb
@@ -4,6 +4,9 @@ class DeviseCreateTeams < ActiveRecord::Migration[6.0]
   def change
     create_table :teams do |t|
       ## Database authenticatable
+      t.string :name, null: false
+      t.string :rep_name, null: false
+      t.integer :started, null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
