e96a412ae93903c136f60631ac050367cb92633d
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-03-10 20:25:01 +0900

    memberモデルのマイグレーションファイル作成

--- db/migrate/20210310112013_create_members.rb
+++ db/migrate/20210310112013_create_members.rb
@@ -1,7 +1,12 @@
 class CreateMembers < ActiveRecord::Migration[6.0]
   def change
     create_table :members do |t|
-
+      t.string :name, null: false
+      t.integer :uni_no, null: false
+      t.integer :age, null: false
+      t.integer :position_id, null:false
+      t.integer :base_hist_id
+      t.references :team, foreign_key: true
       t.timestamps
     end
   end
