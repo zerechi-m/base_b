7925829248fd90b3429cedc76b9a81cad22e0659
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-03-14 18:24:25 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,12 +6,13 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
+    binding.pry
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
     if @message.valid?
       @message.save
-      redirect_to :index
+      redirect_to action: :index
     else
-      @room = Room.find(params[:room_id])
       @messages = @room.messages.includes(:team)
       render :index
     end
@@ -19,6 +20,6 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:content, :game_day, :address)
+    params.require(:message).permit(:content, :game_day, :address,:room_id).merge(team_id: current_team.id)
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../room_message")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
