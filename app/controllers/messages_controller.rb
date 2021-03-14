4eff4bb877bf6c954ab7ff4de8f45ab895204cf4
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-03-14 14:26:36 +0900

    message#createのストロングパラムスを設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,19 @@ class MessagesController < ApplicationController
   end
 
   def create
-    
+    @message = Message.new(message_params)
+    if @message.valid?
+      @message.save
+      redirect_to :index
+    else
+      @room = Room.find(params[:room_id])
+      @messages = @room.messages.includes(:team)
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :game_day, :address)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :team
 
-  validates :content, presence: true
+  validates :content, presence: true 
 end
