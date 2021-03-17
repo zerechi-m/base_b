84b0e5f197db5e366f3238737ae4f1be5d80573e
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-03-17 15:47:18 +0900

    team_gameモデルの作成・各アソシエーション

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,4 @@
 class Game < ApplicationRecord
+  has_many :teams, through: :team_games
+  has_many :team_games, dependent: :destroy
 end
--- db/migrate/20210317060330_create_games.rb
+++ db/migrate/20210317060330_create_games.rb
@@ -1,7 +1,7 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
-
+      t.references :team, foreign_key: :true
       t.timestamps
     end
   end
